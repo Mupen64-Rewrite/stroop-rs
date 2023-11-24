96619bf40815f0fb2744700d69652bf3e2791a69
Eddio0141 <eddio0141@gmail.com>
2023-11-24 17:32:28 +0000

    Revert "have a container the wraps sm64 types"
    This reverts commit 3f64b1d734058e86613440d2ff90f1ae3da72bc4.

--- src/containers/mod.rs
+++ src/containers/mod.rs
@@ -1,39 +1,6 @@
 //! Module for giving interfaces to reading / writing in SM64.
 
-use self::{
-    emulator::EmulatorMemory,
-    map_file::MapFile,
-    sm64_types::{types::*, SM64Container},
-};
-
 mod emulator;
-mod map_file;
-mod sm64_types;
+pub mod map_file;
+pub mod sm64_types;
 mod types;
-
-/// Represents the state of SM64 in an emulator with a specific map file that represents the offsets of types in SM64.
-struct SM64State {
-    emulator: EmulatorMemory,
-    map_file: MapFile,
-    pub mario: Mario,
-}
-
-impl SM64State {
-    pub fn new(emulator: EmulatorMemory, map_file: MapFile) -> Self {
-        Self {
-            emulator,
-            map_file,
-            mario: Mario::default(),
-        }
-    }
-
-    /// Updates the internal state of SM64 by reading from memory.
-    pub fn update(&mut self) {
-        self.mario.read_from_memory(&self.map_file, &self.emulator);
-    }
-
-    /// Writes the internal state of SM64 to memory.
-    pub fn write(&mut self) {
-        self.mario.write_to_memory(&self.map_file, &self.emulator);
-    }
-}
--- src/containers/sm64_types/types.rs
+++ src/containers/sm64_types/types.rs
@@ -1,22 +1,34 @@
 //! Contains wrapped types that interacts with memory in SM64
 
-use crate::containers::types::Padding;
+use crate::containers::{map_file::*, types::Padding};
 
 use super::*;
 
 #[repr(C)]
 #[derive(Default)]
 pub struct Vec3 {
-    pub x: f32,
-    pub y: f32,
-    pub z: f32,
+    x: f32,
+    y: f32,
+    z: f32,
 }
 
 #[repr(C)]
 #[derive(Default)]
-pub struct Mario {
+struct Mario {
     _pad0: Padding<0x3C>,
-    pub pos: Vec3,
+    pos: Vec3,
+}
+
+impl Mario {
+    fn get_pos(&self) -> &Vec3 {
+        &self.pos
+    }
+
+    #[must_use]
+    fn set_pos(&mut self, pos: Vec3) -> PendingWrite<Self> {
+        self.pos = pos;
+        PendingWrite(self)
+    }
 }
 
 impl SM64Container for Mario {}
