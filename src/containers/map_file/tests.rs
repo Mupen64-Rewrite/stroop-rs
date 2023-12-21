66604066c84ff1e7504cead821b6646fdaeef499
Eddio0141 <eddio0141@gmail.com>
2023-12-21 17:18:23 +0000

    test invalid offset

--- src/containers/map_file/tests.rs
+++ src/containers/map_file/tests.rs
@@ -1,10 +1,19 @@
 use crate::containers::{map_file::MapFile, sm64_types::BaseType};
 
 const SM64_US_MAP: &str = include_str!("../../map_files/sm64.us.map");
+const FAKE_MAP: &str = include_str!("tests.rs");
 
 #[test]
 fn parse_sm64_us_map() {
     let map = MapFile::new(SM64_US_MAP);
 
     assert_eq!(map.get_offset(BaseType::gMarioStates), Some(0x33b170));
+}
+
+#[test]
+fn invalid_offset() {
+    // dummy file
+    let map = MapFile::new(FAKE_MAP);
+
+    assert_eq!(map.get_offset(BaseType::gMarioStates), None);
 }
