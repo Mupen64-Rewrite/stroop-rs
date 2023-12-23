09e671e25c3caf65880e027ac842b4d40f5b0d56
Eddio0141 <eddio0141@gmail.com>
2023-12-23 08:21:25 +0000

    added debug

--- src/containers/types.rs
+++ src/containers/types.rs
@@ -1,3 +1,5 @@
+use std::fmt::Debug;
+
 /// A simple padding type to fill in struct with padding bytes.
 ///
 /// # Generic
@@ -9,5 +11,11 @@ pub struct Padding<const N: usize>([u8; N]);
 impl<const N: usize> Default for Padding<N> {
     fn default() -> Self {
         Self([0; N])
+    }
+}
+
+impl<const N: usize> Debug for Padding<N> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "[padding]")
     }
 }
