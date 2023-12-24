e19ef455feba76444bbdb807c6e08ad0b39c42ac
Eddio0141 <eddio0141@gmail.com>
2023-12-24 00:59:26 +0000

    new tests for newlines

--- src/tests/map_file_parse.rs
+++ src/tests/map_file_parse.rs
@@ -1,7 +1,8 @@
 use crate::{containers::sm64_types::BaseType, map_file::MapFile};
 
 const SM64_US_MAP: &str = include_str!("../map_files/sm64.us.map");
-const FAKE_MAP: &str = include_str!("map_file_parse.rs");
+const SM64_US_MAP_WIN_NEWLINE: &str = "0x000000008033b171                gMarioStatess\r\n0x000000008033b170                gMarioStates\r\n";
+const SM64_US_MAP_UNIX_NEWLINE: &str = "0x000000008033b171                gMarioStatess\n0x000000008033b170                gMarioStates\n";
 
 #[test]
 fn parse_sm64_us_map() {
@@ -11,9 +12,23 @@ fn parse_sm64_us_map() {
 }
 
 #[test]
+fn parse_sm64_us_map_win() {
+    let map = MapFile::new(SM64_US_MAP_WIN_NEWLINE);
+
+    assert_eq!(map.get_offset(BaseType::gMarioStates), Some(0x33b170));
+}
+
+#[test]
+fn parse_sm64_us_map_unix() {
+    let map = MapFile::new(SM64_US_MAP_UNIX_NEWLINE);
+
+    assert_eq!(map.get_offset(BaseType::gMarioStates), Some(0x33b170));
+}
+
+#[test]
 fn invalid_offset() {
     // dummy file
-    let map = MapFile::new(FAKE_MAP);
+    let map = MapFile::new("foo");
 
     assert_eq!(map.get_offset(BaseType::gMarioStates), None);
 }
