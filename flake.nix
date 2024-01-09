60d55933d129fdf96bffdbb2aa63a9aae62f81cd
Eddio0141 <eddio0141@gmail.com>
2024-01-09 03:01:28 +0000

    properly wrote package

--- flake.nix
+++ flake.nix
@@ -4,24 +4,23 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = { flake-parts, ... } @ inputs:
+  outputs = { self, flake-parts, ... } @ inputs:
+  let
+    libs = pkgs: with pkgs; [
+      wayland
+      libxkbcommon
+    ];
+  in
     flake-parts.lib.mkFlake { inherit inputs; }
     {
       systems = [ "x86_64-linux" "aarch64-linux" "i686-linux" ];
 
-      perSystem = { pkgs, ... }: {
-        packages.default = pkgs.callPackage ./package.nix {};
-        devShells.default = pkgs.mkShell {};
+      perSystem = { pkgs, ... }: with pkgs; {
+        packages.default = callPackage ./package.nix {};
+        devShells.default = mkShell rec {
+          packages = [ rustc cargo clippy rustfmt rust-analyzer ] ++ libs pkgs;
+          LD_LIBRARY_PATH = lib.makeLibraryPath (libs pkgs);
+        };
       };
-      # languages.rust = {
-      #   enable = true;
-      #   channel = "stable";
-      # };
-
-      # packages = with pkgs; [
-      #   wayland
-      #   libxkbcommon
-      #   libGL
-      # ];
     };
 }
--- package.nix
+++ package.nix
@@ -1,4 +1,19 @@
-{ mkDerivation }:
-mkDerivation {
-  
+{ rustPlatform, lib, wayland, libxkbcommon, makeWrapper }:
+let
+  libs = [ wayland libxkbcommon ];
+in
+rustPlatform.buildRustPackage rec {
+  pname = "stroop-gui";
+  version = "0.1.0";
+  src = ./.;
+  cargoLock.lockFile = ./Cargo.lock;
+  buildAndTestSubdir = "stroop-gui";
+
+  buildInputs = libs;
+  nativeBuildInputs = [ makeWrapper ];
+
+  postInstall = ''
+    mv $out/bin/${pname} $out/bin/${pname}-unwrapped
+    makeWrapper $out/bin/${pname}-unwrapped $out/bin/${pname} --set LD_LIBRARY_PATH ${lib.makeLibraryPath libs}
+  '';
 }
